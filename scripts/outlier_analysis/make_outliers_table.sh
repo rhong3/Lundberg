b62787384427b2d3d0a89b23f75f63bb586b872f
rhong3 <michaelhong1995@gmail.com>
2020-11-27 10:50:34 -0500

    11.27.2

--- scripts/outlier_analysis/make_outliers_table.sh
+++ scripts/outlier_analysis/make_outliers_table.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
 
-gene_column_name="Modified_sequence"
-set="phospho"
+gene_column_name="gene"
+set="BRCA_subtype"
 
-for m in 1-yr-survival 3-yr-survival 5-yr-survival 6-month-survival BRAF NRAS gender PtoM400 PtoM600 stage
-do
-	updown="up"
-	location_of_py_file="make_outliers_table.py"
-	location_of_data_file="/Users/rh2740/documents/Segundo_Melanoma/Results/"$set"/OLA/"$m"/ola_data.csv"
-	iqrs_over_median=1.5 #Note 1.5 IQRs is suggested, this is just for test data.
+updown="up"
+location_of_py_file="make_outliers_table.py"
+location_of_data_file="/Users/rh2740/documents/Lundberg/Results/OLA/"$set"/ola_data.csv"
+iqrs_over_median=1.5 #Note 1.5 IQRs is suggested, this is just for test data.
 
-	output_prefix="/Users/rh2740/documents/Segundo_Melanoma/Results/"$set"/OLA/"$m"/ola_table"
-	sample_names_file="/Users/rh2740/documents/Segundo_Melanoma/Results/"$set"/OLA/"$m"/samples.txt"
-	aggregate=True
-	write_frac_table="True"
+output_prefix="/Users/rh2740/documents/Lundberg/Results/OLA/"$set"/ola_table"
+sample_names_file="/Users/rh2740/documents/Lundberg/Results/OLA/"$set"/samples.txt"
+aggregate=True
+write_frac_table="True"
 
-	python3 ${location_of_py_file} \
-	--input_df ${location_of_data_file} \
-	--iqrs_over_median ${iqrs_over_median} \
-	--gene_column_name ${gene_column_name} \
-	--output_prefix ${output_prefix} \
-	--sample_names_file ${sample_names_file} \
-	--aggregate ${aggregate} \
-	--up_or_down ${updown} \
-	--write_frac_table ${write_frac_table}
-done
+python3 ${location_of_py_file} \
+--input_df ${location_of_data_file} \
+--iqrs_over_median ${iqrs_over_median} \
+--gene_column_name ${gene_column_name} \
+--output_prefix ${output_prefix} \
+--sample_names_file ${sample_names_file} \
+--aggregate ${aggregate} \
+--up_or_down ${updown} \
+--write_frac_table ${write_frac_table}
